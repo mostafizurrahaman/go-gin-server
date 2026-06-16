b9595a7561075527dba7003a6176d13a56b8f34f
mostafizurRahaman <mostafizurrahaman0401@gmail.com>
2026-06-17 00:04:24 +0600

    feat(routes): Create note routes

---
+++ internal/notes/note-routes.go
@@ -0,0 +1,22 @@
+package notes
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+func RegisterNoteRoutes(r *gin.Engine, db *mongo.Database) {
+
+	// ? Create repo first :
+	repo := NewRepo(db)
+
+	// ? Create handler :
+	handler := NewHandler(repo)
+
+	noteGroup := r.Group("notes")
+
+	{
+		noteGroup.POST("/", handler.CreateNote)
+	}
+
+}
