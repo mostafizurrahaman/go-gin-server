17ec38b717bd8b9450a9abf65e1fa0d6ad829ff3
mostafizurRahaman <mostafizurrahaman0401@gmail.com>
2026-06-16 22:40:47 +0600

    feat(note_create): Create note create struct for validation and bindding

--- internal/notes/note-model.go
+++ internal/notes/note-model.go
@@ -10,3 +10,9 @@ type Note struct {
 	CreatedAt time.Time `json:"createdAt" bson:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt" bson:"updatedAt"`
 }
+
+type CreateNewNote struct {
+	Title   string `json:"title" binding:"required,min=3,max=15"`
+	Content string `json:"content" binding:"required,min=3"`
+	Pinned  bool   `json:"pinned"`
+}
